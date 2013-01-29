7e76a02e7734fbc6d541f03e3fa81e5b8e84b33a
Micha Niskin <asdf@ubergibson.com>
2013-01-29 15:00:11 -0500

    worky work

--- src/cljs/alandipert/waffle.cljs
+++ src/cljs/alandipert/waffle.cljs
@@ -4,10 +4,49 @@
    [alandipert.desiderata    :as    d]
    [cljs.core                :as    c])
   (:require-macros
-   [alandipert.waffle.macros :refer [with-let]])
-  (:refer-clojure :exclude [map]))
+   [alandipert.waffle.macros :refer [with-let]]))
+
+(let [rank (atom 0)]
+  (def next-rank
+    "Get the next item in a monotonically increasing sequence of integers."
+    #(swap! rank inc)))
+
+(defn make-node
+  "Idempotently FRP-ize an atom atm."
+  ([atm]
+     (make-node atm (constantly true)))
+  ([atm update-fn]
+     (doto atm
+       (alter-meta! update-in [::sinks] #(or % []))
+       (alter-meta! update-in [::rank] #(or % (next-rank)))
+       (alter-meta! update-in [::update-fn] #(or % update-fn)))))
+
+(defn wrap-update-fn!
+  "Wrap the atom atm's update function with the function f."
+  [atm f]
+  (let [old-update-fn (-> atm meta ::update-fn)
+        new-update-fn #(f old-update-fn)]
+    (doto atm
+      (alter-meta! update-in [::update-fn] new-update-fn))))
+
+(defn increase-sink-ranks!
+  "Walk source's sinks in rank order and increase the rank of each."
+  [source]
+  (doseq [dep (d/bf-seq identity (comp ::sinks meta) source)]
+    (alter-meta! dep assoc-in [::rank] (next-rank))))
+
+(defn attach!
+  "Attaches sink to one or more atoms."
+  [atoms sink]
+  (with-let [attached-sink sink]
+    (doseq [source (map make-node atoms)]
+      (alter-meta! source update-in [::sinks] conj sink)
+      (if (> (-> source meta ::rank) (-> sink meta ::rank))
+        (increase-sink-ranks! source)))))
 
 (defn propagate!
+  "Initiate the FRP evaluation process. The atom atm must be an input cell.
+  Updates are propagated through the dependency graph in topological order."
   [atm]
   (loop [queue (priority-map atm (-> atm meta ::rank))]
     (when (seq queue)
@@ -17,67 +56,24 @@
                  (reduce #(assoc %1 %2 (-> %2 meta ::rank)) remainder (-> node meta ::sinks))
                  remainder))))))
 
-(let [rank (atom 0)]
-  (def next-rank #(swap! rank inc)))
-
-(defn make-node
-  "Idempotently FRP-ize an atom."
-  ([atm]
-     (make-node atm (constantly true)))
-  ([atm update-fn]
-     (doto atm
-       (alter-meta! update-in [::sinks] #(or % []))
-       (alter-meta! update-in [::rank] #(or % (next-rank)))
-       (alter-meta! update-in [::update-fn] #(or % update-fn)))))
-
-(defn increase-sink-ranks!
-  "Walk source's sinks in rank order and increase the rank of each."
-  [source]
-  (doseq [dep (d/bf-seq identity (comp ::sinks meta) source)]
-    (alter-meta! dep assoc-in [::rank] (next-rank))))
-
-(defn add-propagator!
-  "Attaches a watch to atm keyed by sink.  If repeats? is true,
-  all values are repeated.  This should only be called on input
-  atoms."
-  [atm sink repeats?]
-  (add-watch atm sink (fn [_ _ old new]
-                        (if repeats?
-                          (propagate! sink)
-                          (and (not= old new) (propagate! sink))))))
-
 (defn input
+  "FRP-ize an atom, making it an 'input cell'."
   [atm]
-  (with-let [input atm]
-    (alter-meta! input assoc ::input? true)))
-
-(defn attach!
-  "Attaches sink to one or more atoms.  If filter-repeats? is true,
-  only new values from the atoms are propagated to sink."
-  [atoms sink repeats?]
-  (with-let [attached-sink sink]
-    (doseq [source (c/map make-node atoms)]
-      (alter-meta! source update-in [::sinks] conj sink)
-      (if (> (-> source meta ::rank) (-> sink meta ::rank))
-        (increase-sink-ranks! source))
-      (if (-> source meta ::input?)
-        (add-propagator! source sink repeats?)))))
+  (if (not (-> atm meta ::sinks))
+    (doto atm
+      (add-watch ::propagate (fn [_ atm _ _] (propagate! atm)))
+      make-node)
+    (throw (Exception. "Atom is already an FRP cell!"))))
 
 (defn lift
+  "Given a function f or an atom containing a function f, returns a function
+  that returns an FRP-ized atom whose value is always f applied to the derefed
+  values of its arguments."
   [f]
   (fn [& atoms]
-    (let [update #(apply (if (fn? f) f @f) (c/map deref atoms))]
+    (let [update #(apply (if (fn? f) f @f) (map deref atoms))]
       (with-let [lifted (atom (update))]
-        (attach! atoms
-                 (make-node lifted #(reset! lifted (update)))
-                 false)))))
-
-(defn map
-  [source f]
-  (with-let [sink (atom nil)]
-    (attach! [source]
-             (make-node sink #(reset! sink (f @source)))
-             true)))
+        (attach! atoms (make-node lifted #(reset! lifted (update))))))))
 
 ;;; Example
 
