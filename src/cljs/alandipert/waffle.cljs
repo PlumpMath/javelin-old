7672b2e96be85bda23fd2830a30740a169971cb6
Alan Dipert <alan@dipert.org>
2013-01-29 08:58:55 -0500

    computing

--- src/cljs/alandipert/waffle.cljs
+++ src/cljs/alandipert/waffle.cljs
@@ -2,122 +2,68 @@
   (:require
    [alandipert.priority-map  :refer [priority-map]]
    [alandipert.desiderata    :as    d]
-   [cljs.core                :as    cljs])
+   [cljs.core                :as    c])
   (:require-macros
    [alandipert.waffle.macros :refer [with-let]])
   (:refer-clojure :exclude [map]))
 
-;;; Internals
-
-(let [rank  (atom 0)
-      stamp (atom 0)]
-  (def next-rank  #(swap! rank inc))
-  (def next-stamp #(swap! stamp inc)))
-
-(defprotocol INode "Base functionality of either EventStream or
-Behavior.  The graph is modeled with objects containing atoms
-instead of with just atoms.  This makes it easier for consumers to
-extend types used here."
-             (-sinks [_] "Atom of a collection of INodes")
-             (-rank [_] "Atom of a number")
-             (-update-fn [_] "Atom of function to invoke with new pulses."))
-
-(defrecord EventStream [sinks rank update-fn]
-  INode
-  (-sinks [_] sinks)
-  (-rank [_] rank)
-  (-update-fn [_] update-fn))
-
-(defrecord Behavior [sinks rank update-fn last-value]
-  INode
-  (-sinks [_] sinks)
-  (-rank [_] rank)
-  (-update-fn [_] update-fn)
-
-  cljs.core/IDeref
-  (-deref [_] @last-value))
-
-(defn make-pulse
-  [value]
-  {:stamp (next-stamp) :value value})
-
-(defn make-event-stream
-  [sinks update-fn]
-  (EventStream. (atom sinks)
-                (atom (next-rank))
-                (atom update-fn)))
-
-(defn behavior-fn
-  "The difference between Behaviors and EventStreams is that behaviors
-  only propagate when the new value differs from the Behavior's last
-  value.
-
-  update-fn is wrapped with a function returning the ::halt sentinel
-  if the value is not new."
-  [behavior update-fn]
-  (fn [pulse]
-    (if (not= (:value pulse) @behavior)
-      (@(-update-fn behavior) pulse)
-      ::halt)))
-
-(defn make-behavior
-  [sinks update-fn init-value]
-  (with-let [b (Behavior. (atom sinks)
-                            (atom (next-rank))
-                            (atom nil)
-                            (atom init-value))]
-    (reset! (-update-fn b) (behavior-fn b update-fn))))
-
 (defn propagate!
-  [node pulse]
-  (loop [queue (priority-map {:node node :pulse pulse} @(-rank node))]
-    (if (seq queue)
-      (let [{:keys [node pulse]} (key (peek queue))
-            next-pulse (@(-update-fn node) pulse)]
-        (if (not= next-pulse ::halt)
-          (recur (reduce #(assoc %1 {:node %2 :pulse next-pulse} @(-rank %2))
-                         (pop queue)
-                         @(-sinks node))))))))
+  [atm]
+  (loop [queue (priority-map atm (-> atm meta ::rank))]
+    (when (seq queue)
+      (let [node (key (peek queue))
+            remainder (pop queue)]
+        (recur (if (not= ::halt ((-> node meta ::update-fn)))
+                 (reduce #(assoc %1 %2 (-> %2 meta ::rank)) remainder (-> node meta ::sinks))
+                 remainder))))))
 
-(defn add-sink!
-  [node dependent]
-  (swap! (-sinks node) conj dependent)
-  (if (> @(-rank node) @(-rank dependent))
-    (doseq [dep (d/bf-seq identity (comp deref -sinks) dependent)]
-      (reset! (-rank dep) (next-rank)))))
+(let [rank (atom 0)]
+  (def next-rank #(swap! rank inc)))
 
-(defn remove-sink!
-  [node dependent]
-  (swap! (-sinks node) #(filterv (partial not= dependent) %)))
+(defn attach!
+  [source sink]
+  (alter-meta! source update-in [::sinks] (fnil conj []) sink)
+  (if (> (-> source meta ::rank) (-> sink meta ::rank))
+    (doseq [dep (d/bf-seq identity (comp ::sinks meta) sink)]
+      (alter-meta! dep assoc-in [::rank] (next-rank)))))
 
-(defn event-stream
-  [nodes update-fn]
-  (with-let [e (make-event-stream [] update-fn)]
-    (doseq [node nodes]
-      (add-sink! node e))))
+(defn make-node
+  [atm update-fn]
+  (doto atm
+    (alter-meta! assoc
+                 ::rank (next-rank)
+                 ::sinks []
+                 ::update-fn update-fn)))
 
-(defn internal
-  [depends-on]
-  (event-stream depends-on identity))
-
-;;; API
-
-(def receiver internal)
-
-(defn send-event!
-  [node value]
-  (propagate! node (make-pulse value)))
+(defn behavior
+  [init]
+  (let [atm (atom init)]
+    (doto atm
+      (make-node (constantly true))
+      (add-watch ::propagate (fn [& _] (propagate! atm))))))
 
 (defn map
-  [e f]
-  (event-stream [e] #(update-in % [:value] f)))
+  [source f]
+  (with-let [sink (atom nil)]
+    (make-node sink #(f @source))
+    (attach! source sink)))
+
+(defn lift
+  [f]
+  (fn [& atoms]
+    (let [update #(apply f (c/map deref atoms))]
+      (with-let [lifted (atom (update))]
+        (make-node lifted (comp (partial reset! lifted) update))
+        (doseq [a atoms] (attach! a lifted))))))
 
 ;;; Example
 
 (defn doit []
-  (let [e1 (receiver)
-        e2 (-> e1
-               (map #(.toUpperCase %))
-               (map #(js/alert %)))]
-    (doseq [c "omg"]
-      (send-event! e1 c))))
+  (let [n1 (behavior 0)
+        n2 (behavior 0)
+        sum ((lift +) n1 n2)]
+    (map sum #(.log js/console (str "new sum: " %)))
+    (.log js/console (str "starting sum: " @sum))
+
+    (js/setInterval #(swap! n1 inc) 300)
+    (js/setInterval #(swap! n2 inc) 500)))
