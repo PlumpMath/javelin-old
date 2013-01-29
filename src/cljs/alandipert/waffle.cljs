377016a7e527cab31b9ce4d06412620374d0ff9f
Alan Dipert <alan@dipert.org>
2013-01-29 09:36:57 -0500

    aoir

--- src/cljs/alandipert/waffle.cljs
+++ src/cljs/alandipert/waffle.cljs
@@ -1,7 +1,7 @@
 (ns alandipert.waffle
   (:require
    [alandipert.priority-map  :refer [priority-map]]
-   [alandipert.desiderata    :refer [bf-seq]]
+   [alandipert.desiderata    :as    d]
    [cljs.core                :as    c])
   (:require-macros
    [alandipert.waffle.macros :refer [with-let]])
@@ -24,43 +24,50 @@
   [source sink]
   (alter-meta! source update-in [::sinks] (fnil conj []) sink)
   (if (> (-> source meta ::rank) (-> sink meta ::rank))
-    (doseq [dep (bf-seq identity (comp ::sinks meta) source)]
+    (doseq [dep (d/bf-seq identity (comp ::sinks meta) sink)]
       (alter-meta! dep assoc-in [::rank] (next-rank)))))
 
-(defn make-node!
+(defn make-node
   [atm update-fn]
   (doto atm
     (alter-meta! assoc
                  ::rank (next-rank)
                  ::sinks []
-                 ::update-fn update-fn)
-    (add-watch ::propagate (fn [& _] (propagate! atm)))))
+                 ::update-fn update-fn)))
 
 (defn behavior
   [init]
-  (make-node! (atom init) (constantly true)))
+  (let [atm (atom init)]
+    (doto atm
+      (make-node (constantly true))
+      (add-watch ::propagate (fn [& _] (propagate! atm))))))
+
+(defn receiver
+  []
+  (behavior ::none-yet))
 
 (defn map
   [source f]
   (with-let [sink (atom nil)]
-    (make-node! sink #(f @source))
+    (make-node sink #(f @source))
     (attach! source sink)))
 
 (defn lift
   [f]
   (fn [& atoms]
-    (let [update #(apply f (c/map deref atoms))]
+    (let [update #(apply (if (fn? f) f @f) (c/map deref atoms))]
       (with-let [lifted (atom (update))]
-        (make-node! lifted (comp (partial reset! lifted) update))
+        (make-node lifted (comp (partial reset! lifted) update))
         (doseq [a atoms] (attach! a lifted))))))
 
 ;;; Example
 
 (defn doit []
-  (let [n1 (behavior 0)
-        n2 (behavior 0)
-        sum ((lift +) n1 n2)]
-    (map sum #(.log js/console (str "new sum: " %)))
-    (.log js/console (str "starting sum: " @sum))
-    (js/setInterval #(swap! n1 inc) 300)
-    (js/setInterval #(swap! n2 inc) 500)))
+  (let [op (behavior +)
+        n1 (receiver)
+        n2 (behavior 42)
+        sum ((lift op) n1 n2)]
+    (map sum #(.write js/document %))
+    (js/setInterval #(reset! n1 35) 300) ;reset! is like send! kinda
+    (js/setInterval #(reset! n1 23) 100)
+    (js/setInterval #(swap! n2 inc) 400)))
