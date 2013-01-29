4c6b33bb0b2f145fd281544baafcbd00e9b6a6c9
Alan Dipert <alan@dipert.org>
2013-01-29 09:12:20 -0500

    attach! bug

--- src/cljs/alandipert/waffle.cljs
+++ src/cljs/alandipert/waffle.cljs
@@ -1,7 +1,7 @@
 (ns alandipert.waffle
   (:require
    [alandipert.priority-map  :refer [priority-map]]
-   [alandipert.desiderata    :as    d]
+   [alandipert.desiderata    :refer [bf-seq]]
    [cljs.core                :as    c])
   (:require-macros
    [alandipert.waffle.macros :refer [with-let]])
@@ -24,10 +24,10 @@
   [source sink]
   (alter-meta! source update-in [::sinks] (fnil conj []) sink)
   (if (> (-> source meta ::rank) (-> sink meta ::rank))
-    (doseq [dep (d/bf-seq identity (comp ::sinks meta) sink)]
+    (doseq [dep (bf-seq identity (comp ::sinks meta) source)]
       (alter-meta! dep assoc-in [::rank] (next-rank)))))
 
-(defn make-node
+(defn make-node!
   [atm update-fn]
   (doto atm
     (alter-meta! assoc
@@ -38,12 +38,12 @@
 
 (defn behavior
   [init]
-  (make-node (atom init) (constantly true)))
+  (make-node! (atom init) (constantly true)))
 
 (defn map
   [source f]
   (with-let [sink (atom nil)]
-    (make-node sink #(f @source))
+    (make-node! sink #(f @source))
     (attach! source sink)))
 
 (defn lift
@@ -51,7 +51,7 @@
   (fn [& atoms]
     (let [update #(apply f (c/map deref atoms))]
       (with-let [lifted (atom (update))]
-        (make-node lifted (comp (partial reset! lifted) update))
+        (make-node! lifted (comp (partial reset! lifted) update))
         (doseq [a atoms] (attach! a lifted))))))
 
 ;;; Example
@@ -63,5 +63,8 @@
     (map sum #(.log js/console (str "new sum: " %)))
     (.log js/console (str "starting sum: " @sum))
 
-    (js/setInterval #(swap! n1 inc) 300)
-    (js/setInterval #(swap! n2 inc) 500)))
+    (doseq [s (bf-seq identity (comp ::sinks meta) n1)]
+      (.log js/console (pr-str (meta s))))
+;    (js/setInterval #(swap! n1 inc) 300)
+;    (js/setInterval #(swap! n2 inc) 500)
+    ))
