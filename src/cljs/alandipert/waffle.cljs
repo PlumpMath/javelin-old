ab3c9e2d7aed442f187e366fc1a343d1c11875db
Alan Dipert <alan@dipert.org>
2013-01-29 12:13:32 -0500

    bug: map f is getting called twice per event

--- src/cljs/alandipert/waffle.cljs
+++ src/cljs/alandipert/waffle.cljs
@@ -20,53 +20,60 @@
 (let [rank (atom 0)]
   (def next-rank #(swap! rank inc)))
 
-(defn attach!
-  [source sink]
-  (alter-meta! source update-in [::sinks] (fnil conj []) sink)
-  (if (> (-> source meta ::rank) (-> sink meta ::rank))
-    (doseq [dep (d/bf-seq identity (comp ::sinks meta) source)]
-      (alter-meta! dep assoc-in [::rank] (next-rank)))))
-
 (defn make-node
-  [atm update-fn]
-  (doto atm
-    (alter-meta! assoc
-                 ::rank (next-rank)
-                 ::sinks []
-                 ::update-fn update-fn)))
+  "Idempotently FRP-ize an atom."
+  ([atm]
+     (make-node atm (constantly true)))
+  ([atm update-fn]
+     (doto atm
+       (alter-meta! update-in [::sinks] #(or % []))
+       (alter-meta! update-in [::rank] #(or % (next-rank)))
+       (alter-meta! update-in [::update-fn] #(or % update-fn)))))
 
-(defn behavior
-  [init]
-  (let [atm (atom init)]
-    (doto atm
-      (make-node (constantly true))
-      (add-watch ::propagate (fn [& _] (propagate! atm))))))
+(defn increase-sink-ranks!
+  "Walk source's sinks in rank order and increase the rank of each."
+  [source]
+  (doseq [dep (d/bf-seq identity (comp ::sinks meta) source)]
+    (alter-meta! dep assoc-in [::rank] (next-rank))))
 
-(defn receiver
-  []
-  (behavior ::none-yet))
+(defn add-propagator!
+  "Attaches a watch to atm keyed by sink.  If filter-repeats? is true,
+  only new values are propagated."
+  [atm sink filter-repeats?]
+  (add-watch atm sink (fn [k ref old new]
+                        (if filter-repeats?
+                          (and (not= old new) (propagate! sink))
+                          (propagate! sink)))))
 
-(defn map
-  [source f]
-  (with-let [sink (atom nil)]
-    (make-node sink #(f @source))
-    (attach! source sink)))
+(defn attach!
+  "Attaches sink to one or more atoms.  If filter-repeats? is true,
+  only new values from the atoms are propagated to sink."
+  [atoms sink filter-repeats?]
+  (with-let [attached-sink sink]
+    (doseq [source (c/map make-node atoms)]
+      (alter-meta! source update-in [::sinks] conj sink)
+      (if (> (-> source meta ::rank) (-> sink meta ::rank))
+        (increase-sink-ranks! source))
+      (add-propagator! source sink filter-repeats?))))
 
 (defn lift
   [f]
   (fn [& atoms]
     (let [update #(apply (if (fn? f) f @f) (c/map deref atoms))]
       (with-let [lifted (atom (update))]
-        (make-node lifted (comp (partial reset! lifted) update))
-        (doseq [a atoms] (attach! a lifted))))))
+        (attach! atoms (make-node lifted #(reset! lifted (update))) true)))))
+
+(defn map
+  [source f]
+  (with-let [sink (atom nil)]
+    (attach! [source] (make-node sink #(reset! sink (f @source))) false)))
 
 ;;; Example
 
 (defn doit []
-  (let [n1 (receiver)
-        n2 (behavior 42)
+  (let [n1 (atom 0)
+        n2 (atom 0)
         sum ((lift +) n1 n2)]
     (map sum #(.write js/document %))
-    (js/setInterval #(reset! n1 35) 300) ;reset! is like send! kinda
-    (js/setInterval #(reset! n1 23) 100)
-    (js/setInterval #(swap! n2 inc) 400)))
+    ;; Meanwhile...
+    (.setInterval js/window #(swap! n1 inc) 1000)))
