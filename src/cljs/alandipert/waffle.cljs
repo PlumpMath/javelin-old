490302f1743ada376881f90cf948090aa2820e92
Alan Dipert <alan@dipert.org>
2013-01-29 19:41:04 -0500

    wip

--- src/cljs/alandipert/waffle.cljs
+++ src/cljs/alandipert/waffle.cljs
@@ -1,8 +1,7 @@
 (ns alandipert.waffle
   (:require
    [alandipert.priority-map  :refer [priority-map]]
-   [alandipert.desiderata    :as    d]
-   [cljs.core                :as    c])
+   [alandipert.desiderata    :as    d])
   (:require-macros
    [alandipert.waffle.macros :refer [with-let]]))
 
@@ -11,76 +10,73 @@
     "Get the next item in a monotonically increasing sequence of integers."
     #(swap! rank inc)))
 
-(defn make-node
-  "Idempotently FRP-ize an atom atm."
+(defn make-input-cell
+  "Idempotently mutate the atom atm, adding FRP cell metadata."
   ([atm]
-     (make-node atm (constantly true)))
+     (make-input-cell atm (constantly true)))
   ([atm update-fn]
      (doto atm
        (alter-meta! update-in [::sinks] #(or % []))
        (alter-meta! update-in [::rank] #(or % (next-rank)))
        (alter-meta! update-in [::thunk] #(or % update-fn)))))
 
-(defn wrap-thunk!
-  "Wrap the atom atm's update function with the function f."
-  [atm f]
-  (let [old-thunk (-> atm meta ::thunk)
-        new-thunk #(f old-thunk)]
-    (doto atm
-      (alter-meta! update-in [::thunk] new-thunk))))
+(defn make-formula-cell
+  [& args]
+  (with-let [cell (apply make-input-cell args)]
+    (set-validator! cell #(= (-> ::lifted meta %) (-> ::rank meta cell)))))
 
 (defn increase-sink-ranks!
-  "Walk source's sinks in rank order and increase the rank of each."
+  "Preorder traversal of tree rooted at source, increasing the rank of
+  all descendents. This ensures that all sinks have higher ranks than
+  their sources---a necessary invariant for glitch elimination."
   [source]
   (doseq [dep (d/bf-seq identity (comp ::sinks meta) source)]
     (alter-meta! dep assoc-in [::rank] (next-rank))))
 
 (defn attach!
-  "Attaches sink to one or more atoms."
-  [atoms sink]
+  "Attaches a sink cell to one or more source cells. This creates the
+  dependency graph that is used for propagation."
+  [sources sink]
   (with-let [attached-sink sink]
-    (doseq [source (map make-node atoms)]
+    (doseq [source (map make-input-cell sources)]
       (alter-meta! source update-in [::sinks] conj sink)
       (if (> (-> source meta ::rank) (-> sink meta ::rank))
         (increase-sink-ranks! source)))))
 
 (defn propagate!
   "Initiate the FRP evaluation process. The atom atm must be an input cell.
-  Updates are propagated through the dependency graph in topological order."
+  Updates are propagated through the dependency graph in rank order."
   [atm]
   (loop [queue (priority-map atm (-> atm meta ::rank))]
     (when (seq queue)
       (let [node (key (peek queue))
             remainder (pop queue)]
         (recur (if (not= ::halt ((-> node meta ::thunk)))
-                 (reduce #(assoc %1 %2 (-> %2 meta ::rank)) remainder (-> node meta ::sinks))
+                 (reduce #(assoc %1 %2 (-> %2 meta ::rank))
+                         remainder
+                         (-> node meta ::sinks))
                  remainder))))))
 
 (defn input
-  "FRP-ize an atom, making it an 'input cell'."
+  "FRP-ize an atom, making it an input cell. Input cells accept arbitrary
+  values and are manipulated using the normal swap! and reset!."
   [atm]
   (if (-> atm meta ::sinks)
     (throw (js/Error. "Atom is already an FRP cell!"))
     (doto atm
       (add-watch ::propagate (fn [_ atm _ _] (propagate! atm)))
-      make-node)))
+      make-input-cell)))
 
 (defn lift
-  "Given a function f or an atom containing a function f, returns a function
-  that returns an FRP-ized atom whose value is always f applied to the derefed
-  values of its arguments."
+  "Given a function f or a cell containing a function f, returns a function
+  that returns a formula cell whose value is always f applied to the derefed
+  values of its arguments. Formula cells cannot be updated directly using
+  swap! or reset!."
   [f]
   (fn [& atoms]
     (let [update #(apply (if (fn? f) f @f) (map deref atoms))]
       (with-let [lifted (atom (update))]
-        (attach! atoms (make-node lifted #(reset! lifted (update))))))))
+        (->> #(reset! lifted (with-meta {::lifted (-> ::rank meta lifted)} (update)))
+             (make-formula-cell lifted)
+             (attach atoms))))))
 
-;;; Example
-
-(defn doit []
-  (let [n1 (input (atom 0))
-        n2 (atom 0)
-        sum ((lift +) n1 n2)]
-    (map (map sum identity) #(.write js/document %))
-    ;; Meanwhile...
-    (.setInterval js/window #(swap! n1 inc) 1000)))
--- src/cljs/alandipert/waffle/b.cljs
+++ src/cljs/alandipert/waffle/b.cljs
@@ -5,8 +5,9 @@
   (:refer-clojure :exclude [map identity filter])
   (:require-macros [alandipert.waffle.macros :refer [maptemplate]]))
 
+
 (defn no-repeats
-  "Stops propagation of repeated values from this FRP atom."
+  "Returns the value of the atom whenever it changes."
   [atm]
   (doto atm
     (w/wrap-thunk!
@@ -19,8 +20,14 @@
               :w/halt))))))
 
 (def lift     (fn [to-lift]
+                (no-repeats to-lift)
                 (fn [& atoms]
-                  (apply w/lift (no-repeats to-lift) atoms))))
+                  (apply w/lift to-lift atoms))))
+
+(defn lift
+  ([f]
+            (fn [& atms]
+              (no-repeats (apply (partial w/lift f) atms)))))
 
 (maptemplate
   (fn [sym] `(def ~sym (~'lift ~(symbol (str 'cljs) (str sym)))))
