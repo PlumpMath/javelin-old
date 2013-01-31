8c30cfc320f5bec350ed52ffcd1639e74e77d330
Alan Dipert <alan@dipert.org>
2013-01-30 19:09:13 -0500

    oopsies

--- src/clj/alandipert/waffle/macros.clj
+++ src/clj/alandipert/waffle/macros.clj
@@ -3,12 +3,13 @@
 
 ;;; Mutation helper macros.
 
+(defmacro with
+  "Evaluates body and returns resource."
+  [resource & body]
+  `(do ~@body ~resource))
+
 (defmacro with-let
   "Binds resource to binding and evaluates body.  Then, returns
   resource.  It's cross between doto and with-open."
   [[binding resource] & body]
   `(let [~binding ~resource] ~@body ~resource))
-
-(defmacro with
-  [resource & body]
-  `(let [ret# ~resource] ~@body ret#))
