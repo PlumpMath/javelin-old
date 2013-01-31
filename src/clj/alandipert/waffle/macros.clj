5c0e15d0411a5941f703aaa17db398a3e97c917b
Alan Dipert <alan@dipert.org>
2013-01-30 19:27:12 -0500

    double-eval is a biznatch

--- src/clj/alandipert/waffle/macros.clj
+++ src/clj/alandipert/waffle/macros.clj
@@ -3,13 +3,13 @@
 
 ;;; Mutation helper macros.
 
-(defmacro with
-  "Evaluates body and returns resource."
-  [resource & body]
-  `(do ~@body ~resource))
-
 (defmacro with-let
   "Binds resource to binding and evaluates body.  Then, returns
   resource.  It's cross between doto and with-open."
   [[binding resource] & body]
-  `(let [~binding ~resource] ~@body ~resource))
+  `(let [~binding ~resource] ~@body ~binding))
+
+(defmacro with
+  "Evaluates body and returns resource."
+  [resource & body]
+  `(with-let [res# ~resource] ~@body))
