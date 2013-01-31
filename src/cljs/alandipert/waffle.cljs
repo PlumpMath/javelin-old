ce7dbf87909e427a64136ed84aa6db4f6e585ba1
Micha Niskin <asdf@ubergibson.com>
2013-01-31 14:43:54 -0500

    attach! detach! and refactor

--- src/cljs/alandipert/waffle.cljs
+++ src/cljs/alandipert/waffle.cljs
@@ -5,6 +5,32 @@
   (:require-macros
    [alandipert.waffle.macros :refer [with with-let]]))
 
+(declare attach! detach!)
+
+;; PREDICATES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn atom?
+  "True if obj is a ClojureScript atom."
+  [obj]
+  (instance? cljs.core/Atom obj))
+
+(defn cell?
+  "True if obj is a ClojureScript atom marked with ::cell metadata."
+  [obj]
+  (and (atom? obj) (-> obj meta ::cell)))
+
+(defn changes?
+  "Is this cell only propagating when the value changes?"
+  [cell]
+  (-> cell meta ::changes))
+
+(defn done?
+  "Is this cell marked as done?"
+  [cell]
+  (-> cell meta ::done))
+
+;; INTERNAL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (let [rank (atom 0)]
   (def next-rank
     "Get the next item in a monotonically increasing sequence of integers."
@@ -14,66 +40,38 @@
 
 (def swapping (atom ::not-swapping))
 
-(defn atom?
-  "True if obj is a ClojureScript atom."
-  [obj]
-  (instance? cljs.core/Atom obj))
-
-(defn deref*
+(defn- deref*
   "Dereferences obj if it satisfies IDeref, otherwise returns obj."
   [obj]
   (if (satisfies? cljs.core/IDeref obj) @obj obj))
 
-(defn cell?
-  "True if obj is a ClojureScript atom marked with ::cell metadata."
-  [obj]
-  (and (atom? obj) (-> obj meta ::cell)))
-
-(defn detached?
-  "Is this cell marked as detached?"
-  [cell]
-  (-> cell meta ::detached))
-
-(defn changes?
-  "Is this cell only propagating when the value changes?"
-  [cell]
-  (-> cell meta ::changes))
-
-(defn detach!
-  "Mark this cell as detached."
-  [cell]
-  (doto cell (alter-meta! assoc-in [::detached] true)))
-
-(defn make-input-cell
+(defn- input-cell
   "Idempotently mutate the atom atm, adding metadata that makes it a cell.
   The pieces of metadata that are added are:
 
   ::cell     - Marker identifying this atom as a cell.
   ::rank     - Numeric ranking that determines evaluation order.
   ::sinks    - Vector of dependent cells.
-  ::detached - True when this cell should not participate in evaluation.
+  ::done     - True when this cell should not participate in evaluation.
   ::silent   - True when this cell should not propagate to its sinks.
   ::thunk    - Thunk to be invoked on evaluation that may return ::none
                and stop propagation to its sinks."
   ([atm]
-     (make-input-cell atm (constantly true)))
+   (input-cell atm nil))
   ([atm thunk]
-   (doto atm (alter-meta! merge {::cell     true
-                                 ::rank     (next-rank)
-                                 ::sinks    []
-                                 ::detached false
-                                 ::silent   false
-                                 ::thunk    thunk}))))
+   (doto atm
+     (alter-meta!
+       merge
+       {::cell     true
+        ::rank     (next-rank) 
+        ::sources  #{}
+        ::sinks    #{} 
+        ::done     false 
+        ::silent   false 
+        ::thunk    (or thunk (constantly nil))})
+     detach!)))
 
-(defn make-formula-cell
-  "Make an input cell and add a validator function to effectively disable
-  swap! and reset!, as the values in formula cells are dictated by the
-  dependency graph and should not be arbitrarily updated."
-  [& args]
-  (with-let [cell (apply make-input-cell args)]
-    (set-validator! cell #(and (not= ::not-swapping %) (= @swapping %)))))
-
-(defn increase-sink-ranks!
+(defn- increase-sink-ranks!
   "Preorder traversal of tree rooted at source cell, increasing the
   rank of all descendents. This ensures that all sinks have higher
   ranks than their sources---a necessary invariant for glitch
@@ -82,17 +80,7 @@
   (doseq [dep (d/bf-seq identity (comp ::sinks meta) source)]
     (alter-meta! dep assoc-in [::rank] (next-rank))))
 
-(defn attach!
-  "Attaches a sink cell to one or more source cells. This creates the
-  dependency graph that is used for propagation."
-  [sources sink]
-  (with sink
-    (doseq [source sources]
-      (alter-meta! source update-in [::sinks] conj sink)
-      (if (> (-> source meta ::rank) (-> sink meta ::rank))
-        (increase-sink-ranks! source)))))
-
-(defn propagate!
+(defn- propagate!
   "Initiate the FRP evaluation process. The atom atm must be an input cell.
   Updates are propagated through the dependency graph in rank order."
   [cell]
@@ -104,21 +92,20 @@
             halt?     #(or (= ::none ((-> cell meta ::thunk)))
                            (-> cell meta ::silent))
             children  (-> cell meta ::sinks)]
-        (if (and (seq children) (every? detached? children))
+        (if (and (seq children) (every? done? children))
           (detach! cell)
           (recur (if-not (halt?)
-                   (reduce q-add siblings (remove detached? children))
+                   (reduce q-add siblings (remove done? children))
                    siblings)))))))
+
+;; CREATE CELL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn input
   "FRP-ize an atom, making it an input cell. Input cells accept arbitrary
   values and are manipulated using the normal swap! and reset!."
   [atm]
   {:pre [(not (cell? atm))]}
-  (let [watch-fn (fn [_ cell _ _]
-                   (if-not (detached? cell)
-                     (propagate! cell)))]
-    (doto atm (add-watch ::propagate watch-fn) make-input-cell)))
+  (input-cell atm))
 
 (defn lift
   "Given a function f or a cell containing a function f, returns a function
@@ -134,10 +121,46 @@
           thunk     #(with-let [value (eval)]
                        (if (not= ::none value) (commit! value)))
           cell-args (filter cell? args)]
-      (->> thunk (make-formula-cell lifted) (attach! args))
+      (->> thunk (input-cell lifted) (attach! args))
       (if (or (empty? cell-args) (every? changes? cell-args))
         (alter-meta! lifted assoc-in [::changes] true)) 
       lifted)))
+
+;; CONTROL PROPAGATION ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn done!
+  "Mark this cell as done."
+  [cell]
+  (doto cell (alter-meta! assoc-in [::done] true)))
+
+(defn detach!
+  "Detaches this cell from its sources and makes it an input cell."
+  [cell]
+  {:pre [(cell? cell)]}
+  (let [watch-fn    (fn [_ cell _ _]
+                      (if-not (done? cell)
+                        (propagate! cell)))
+        remove-this (fn [source]
+                      (-> source
+                        (alter-meta! update-in [::sinks] remove #(= cell %))))]
+    (mapv remove-this (-> cell meta ::sources))
+    (doto cell
+      (alter-meta! assoc-in [::sources] #{})
+      (add-watch ::propagate watch-fn)
+      (set-validator! nil))))
+
+(defn attach!
+  "Attaches a sink cell to one or more source cells and makes it a formula
+  cell. Attaching a sink to a source sets up the dependency graph."
+  [sources sink]
+  (with (doto sink
+          (alter-meta! assoc-in [::sources] (into #{} sources))
+          (remove-watch ::propagate)
+          (set-validator! #(and (not= ::not-swapping %) (= @swapping %))))
+    (doseq [source sources]
+      (alter-meta! source update-in [::sinks] conj sink)
+      (if (> (-> source meta ::rank) (-> sink meta ::rank))
+        (increase-sink-ranks! source)))))
 
 (defn changes
   "Given a cell, returns a cell which only propagates pulses that changed
@@ -154,8 +177,8 @@
         (alter-meta! assoc-in [::changes] true)))))
 
 (defn silence
-  "Cells that have been silenced get updated during propagation but do not
-  propagate pulses to their sinks."
+  "Given a cell, returns a cell which is updated during propagation but does
+  not propagate pulses to their sinks."
   [cell]
   (doto ((lift identity) cell)
     (alter-meta! assoc-in [::silent] true)))
